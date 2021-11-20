acd206f15eeb21dbd44bc0af5ba68ddd8f55ba81
Yash Ray <ray.yash.08@gmail.com>
2021-11-20 14:26:59 +0530

    repo setup done

---
+++ pr_reusable_components/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='instruction'>
+        Create a folder with your Registration Number inside <em>src -{">"}{" "}
+        components</em>, and inside that create your components the way you want
+        to(either a single jsx file for each component, or a folder having js/jsx &
+        css/sass files). Import and add your components below {"<h1>"}.
+      </div>
+    </div>
+  );
+}
+
+export default App;
